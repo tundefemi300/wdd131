d25711a955d1c648bdefc89a7fe9310473d0dd69
tundefemi300 <tundefemi300@gmail.com>
2026-03-20 11:52:07 +0100

    complete week 1 assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Current Year
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+// Last Modified
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
